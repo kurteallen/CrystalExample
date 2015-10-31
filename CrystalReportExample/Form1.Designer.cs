2a99a5e28dd0acd73f291f91f2ebc6f1116f5c9b
kurteallen@yahoo.com <kurteallen@yahoo.com>
2015-10-31 02:47:55 -0400

    Crystal Report First commit

---
+++ CrystalReportExample/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace CrystalReportExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer
+            // 
+            this.crystalReportViewer.ActiveViewIndex = -1;
+            this.crystalReportViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.crystalReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer.Name = "crystalReportViewer";
+            this.crystalReportViewer.Size = new System.Drawing.Size(616, 554);
+            this.crystalReportViewer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 554);
+            this.Controls.Add(this.crystalReportViewer);
+            this.Name = "Form1";
+            this.Text = "Customer Email Report";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer;
+    }
+}
+
---
+++ CrystalReportExample/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.Collections;
+
+namespace CrystalReportExample
+{
+    public partial class Form1 : Form
+    {
+        private const string PARAMETER_FIELD_NAME = "Country";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConfigureCrystalReports();
+            
+            
+            
+            ConnectionInfo connectionInfo = new ConnectionInfo();
+            SetDBLogonForReport(connectionInfo);
+            connectionInfo.DatabaseName = "connex";
+            connectionInfo.UserID = "subroot";
+            connectionInfo.Password = "12CCts#$";
+        }
+
+        private void ConfigureCrystalReports()
+        {
+            //ArrayList arrayList = new ArrayList();
+            //arrayList.Add("Canada");
+            //arrayList.Add("United States");
+
+            string reportPath = Application.StartupPath + "\\" + "CrystalExample.rpt";
+            //string reportPath = "C:\\Users\\sunpack\\Documents\\Visual Studio 2013\\Projects\\CrystalReportExample\\CrystalReportExample\\CrystalExample.rpt";
+            crystalReportViewer.ReportSource = reportPath;
+            //ParameterFields parameterFields = crystalReportViewer.ParameterFieldInfo;
+           // defaultParameterValuesList.DataSource = GetDefaultValuesFromParameterField(parameterFields);
+            //SetCurrentValuesForParameterField(parameterFields, arrayList);
+           
+
+        }
+
+        private void SetDBLogonForReport(ConnectionInfo connectionInfo)
+        {
+            TableLogOnInfos tableLogOnInfos = crystalReportViewer.LogOnInfo;
+            foreach (TableLogOnInfo tableLogOnInfo in tableLogOnInfos)
+            {
+                tableLogOnInfo.ConnectionInfo = connectionInfo;
+            }
+        }
+
+        private ArrayList GetDefaultValuesFromParameterField(ParameterFields parameterFields)
+        {
+            ParameterField parameterField = parameterFields[PARAMETER_FIELD_NAME];
+            ParameterValues defaultParameterValues = parameterField.DefaultValues;
+            ArrayList arrayList = new ArrayList();
+            foreach (ParameterValue parameterValue in defaultParameterValues)
+            {
+                if (!parameterValue.IsRange)
+                {
+                    ParameterDiscreteValue parameterDiscreteValue = (ParameterDiscreteValue)parameterValue;
+                    arrayList.Add(parameterDiscreteValue.Value.ToString());
+                }
+            }
+
+            return arrayList;
+        }
+
+        private void SetCurrentValuesForParameterField(ParameterFields parameterFields, ArrayList arrayList)
+        {
+            ParameterValues currentParameterValues = new ParameterValues();
+            foreach (object submittedValue in arrayList)
+            {
+                ParameterDiscreteValue parameterDiscreteValue = new ParameterDiscreteValue();
+                parameterDiscreteValue.Value = submittedValue.ToString();
+                currentParameterValues.Add(parameterDiscreteValue);
+            }
+            ParameterField parameterField = parameterFields[PARAMETER_FIELD_NAME];
+            parameterField.CurrentValues = currentParameterValues;
+        }
+
+    }
+}
